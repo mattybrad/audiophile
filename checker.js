dfde53db4bb43d1322db21f783015553e70c272d
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2016-05-17 01:57:39 +0100

    basic album checker works

--- checker.js
+++ checker.js
@@ -3,10 +3,39 @@ var path = require("path");
 var id3 = require("id3js");
 var baseFolder = "music";
 
-checkTrack('Bright Eyes', 'Cassadaga', 'Bright Eyes - Hot Knives.mp3', function(result) {
+checkAlbum('Bright Eyes', 'Cassadaga', function(result) {
     console.log("RESULT:");
     console.log(result);
 });
+
+function checkAlbum(artistFolder, albumFolder, callback) {
+    var files = fs.readdirSync(path.join(baseFolder, artistFolder, albumFolder));
+    var tracksToCheck = [];
+    var fileResults = [];
+
+    function onTrackChecked(result) {
+        fileResults.push(result);
+        if(fileResults.length == tracksToCheck.length) callback(fileResults);
+    }
+
+    for(var i = 0; i < files.length; i ++) {
+        switch(path.extname(files[i])) {
+            case ".mp3":
+            case ".wma":
+            case ".flac":
+            case ".wav":
+            tracksToCheck.push(files[i]);
+            break;
+
+            default:
+            // do nothing
+        }
+    }
+
+    for(var i = 0; i < tracksToCheck.length; i ++) {
+        checkTrack(artistFolder, albumFolder, tracksToCheck[i], onTrackChecked);
+    }
+}
 
 function checkTrack(artistFolder, albumFolder, fileName, callback) {
     id3(
