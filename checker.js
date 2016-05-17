5abf80a41d64f2a01ab16905e5a0739b2927ef06
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2016-05-17 01:44:03 +0100

    basic track checker working

--- checker.js
+++ checker.js
@@ -1,3 +1,24 @@
-var fs = require("fs"), path = require("path");
+var fs = require("fs");
+var path = require("path");
+var id3 = require("id3js");
+var baseFolder = "music";
 
-console.log(fs.readdirSync('./music'));
+checkTrack('Bright Eyes', 'Cassadaga', 'Bright Eyes - Hot Knives.mp3', function(result) {
+    console.log("RESULT:");
+    console.log(result);
+});
+
+function checkTrack(artistFolder, albumFolder, fileName, callback) {
+    id3(
+        {
+            file: path.join(baseFolder, artistFolder, albumFolder, fileName),
+            type: id3.OPEN_LOCAL
+        },
+        function(err, tags) {
+            callback({
+                artistMatchesFolder: tags.artist == artistFolder,
+                albumMatchesFolder: tags.album == albumFolder
+            });
+        }
+    );
+}
